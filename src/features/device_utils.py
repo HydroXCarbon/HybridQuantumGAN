49cdbf16868efab7c5a9a65559496f636cf1187c
HydroXCarbon <purin.pongpanich@gmail.com>
2024-09-06 15:49:33 +0700

    QNN-10 Refactor configuration settings and device selection

--- src/features/device_utils.py
+++ src/features/device_utils.py
@@ -4,7 +4,11 @@ from colorama import Fore, Style, init
 def get_device(device):
   if torch.cuda.is_available() and device == 'cuda':
     device = torch.device("cuda")
-    print(Fore.GREEN + 'Using cuda:' + Style.RESET_ALL , torch.cuda.get_device_name(device=device))
+    print(Fore.GREEN + 'Using cuda:' + Style.RESET_ALL, torch.cuda.get_device_name(device=device))
+    return device
+  elif device == 'cpu':
+    device = torch.device("cpu")
+    print(Fore.GREEN+ 'Using cpu:' + Style.RESET_ALL + ' CPU')
     return device
   else:
     device = torch.device("cpu")
