2d5aa8990980efb5ee06da52760ce0b3f63f50f4
HydroXCarbon <purin.pongpanich@gmail.com>
2024-09-11 17:08:45 +0700

    QNN-10 Refactor configuration settings and device selection rename some module for more clearly

--- src/models/FLC_Classical_Discriminator1.py
+++ src/models/FLC_Classical_Discriminator1.py
@@ -1,19 +1,19 @@
 from torch import nn
 
-class ClassicalDiscriminator1(nn.Module):
+class FLC_Classical_Discriminator1(nn.Module):
   def __init__(self):
     super().__init__()
     self.model = nn.Sequential(
       nn.Flatten(),
       nn.Linear(784, 1024),
       nn.ReLU(),
-      nn.Dropout(0.3),
+      nn.Dropout(0.5),
       nn.Linear(1024, 512),
       nn.ReLU(),
-      nn.Dropout(0.3),
+      nn.Dropout(0.5),
       nn.Linear(512, 256),
       nn.ReLU(),
-      nn.Dropout(0.3),
+      nn.Dropout(0.5),
       nn.Linear(256, 1),
       nn.Sigmoid(),
     )
