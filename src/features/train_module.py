60898b42d917baeb6fdccffd4e28a0a7cb29d4a5
HydroXCarbon <purin.pongpanich@gmail.com>
2024-09-11 19:22:03 +0700

    QNN-11 feat: add basic DDP and rename some model

--- src/features/train_module.py
+++ src/features/train_module.py
@@ -1,7 +1,10 @@
 from visualization import PlotTrainingProgress, PlotEvolution
+from torch.nn.parallel import DistributedDataParallel as DDP
 from tqdm import tqdm
 from colorama import Fore, Style
+
 import torch
+import torch.distributed as dist
 import wandb
 
 class LossValues:
@@ -14,7 +17,7 @@ def train_discriminator(discriminator, optimizer_discriminator, all_samples, all
   discriminator.zero_grad()
   optimizer_discriminator.zero_grad()
   output_discriminator = discriminator(all_samples)
-  loss_discriminator = discriminator.loss_function(output_discriminator, all_samples_labels)
+  loss_discriminator = discriminator.module.loss_function(output_discriminator, all_samples_labels)
   loss_discriminator.backward(retain_graph=retain_graph)
   optimizer_discriminator.step()
   return loss_discriminator
@@ -28,15 +31,15 @@ def train_generator(generator, discriminator_list, optimizer_generator, latent_s
     for discriminator in discriminator_list:
       combined_output += discriminator(generated_samples)
     combined_output /= len(discriminator_list)
-    loss_generator = generator.loss_function(combined_output, real_samples_labels)
+    loss_generator = generator.module.loss_function(combined_output, real_samples_labels)
   elif training_mode == 'alternating':
     discriminator = discriminator_list[epoch % num_discriminators]
     output_discriminator_generated = discriminator(generated_samples)
-    loss_generator = generator.loss_function(output_discriminator_generated, real_samples_labels)
+    loss_generator = generator.module.loss_function(output_discriminator_generated, real_samples_labels)
   elif training_mode == 'continuous':
     for discriminator in discriminator_list:
       output_discriminator_generated = discriminator(generated_samples)
-      loss_generator = generator.loss_function(output_discriminator_generated, real_samples_labels)
+      loss_generator = generator.module.loss_function(output_discriminator_generated, real_samples_labels)
   else:
     raise ValueError(f"Training mode {training_mode} not supported")
 
@@ -44,7 +47,9 @@ def train_generator(generator, discriminator_list, optimizer_generator, latent_s
   optimizer_generator.step()
   return loss_generator
 
-def train_model(device, 
+def train_model(rank, 
+                world_size,
+                device, 
                 epochs, 
                 train_loader, 
                 model_list,
@@ -61,31 +66,46 @@ def train_model(device,
   from features import save_checkpoint
   from visualization import generate_sample
 
+  # Initialize DistributedDataParallel
+  dist.init_process_group(backend='gloo', rank=rank, world_size=world_size)
+
   if loss_values is None:
     loss_values = LossValues()
 
+  # Fix bug Optimizer.name disappears
+  for optimizer, model in zip(optimizer_list, model_list):
+    optimizer.name = 'optimizer_' + model.name
+
   # Setup models
   generator = model_list[0]
+  discriminator_list = [discriminator for discriminator in model_list[1:]]
+
+  # Wrap models with DistributedDataParallel
+  generator = DDP(generator)
+  discriminator_list = [DDP(discriminator) for discriminator in discriminator_list]
+
   optimizer_generator = optimizer_list[0]
-  discriminator_list = model_list[1:]
   optimizer_discriminator_list = optimizer_list[1:]
+
   num_discriminators = len(discriminator_list)
   batch_size = train_loader.batch_size
   generated_samples_list = []
   total_batches = len(train_loader)
-      
+    
   # Create instance for plotting
-  plot_progress = PlotTrainingProgress()
-  if show_training_evolution:
-    plot_evolution = PlotEvolution(epochs=epochs-start_epoch)
+  if rank == 0:
+    # Training loop
+    print(Fore.GREEN + "Start training: " + Style.RESET_ALL + f'Epoch {start_epoch}')
+    
+    plot_progress = PlotTrainingProgress()
+    if show_training_evolution:
+      plot_evolution = PlotEvolution(epochs=epochs-start_epoch)
 
-  # Training loop
-  print(Fore.GREEN + "Start training: " + Style.RESET_ALL + f'Epoch {start_epoch}')
-  
   # Initialize progress bar
   progress_bar_epoch = tqdm(total=epochs-start_epoch, desc=f"Model Progress", unit="epoch", leave=True)
   
   for epoch_i, epoch in enumerate(range(start_epoch, epochs)):
+
     # Initialize batch progress bar
     progress_bar_batch = tqdm(total=total_batches, desc=f"Training Epoch {epoch}", unit="batch", leave=False)
       
@@ -104,61 +124,65 @@ def train_model(device,
         loss_discriminator = train_discriminator(discriminator, optimizer_discriminator, all_samples, all_samples_labels, retain_graph)
         loss_discriminator = loss_discriminator.cpu().detach().numpy()
         
-        if discriminator.name not in loss_values.discriminator_loss_values:
-          loss_values.discriminator_loss_values[discriminator.name] = []
-        loss_values.discriminator_loss_values[discriminator.name].append(loss_discriminator)
+        if discriminator.module.name not in loss_values.discriminator_loss_values:
+          loss_values.discriminator_loss_values[discriminator.module.name] = []
+        loss_values.discriminator_loss_values[discriminator.module.name].append(loss_discriminator)
         
         if log_wandb:
-          wandb.log({f"{discriminator.name}_loss": loss_discriminator})
+          wandb.log({f"{discriminator.module.name}_loss": loss_discriminator})
 
       # Training the generator
       loss_generator = train_generator(generator, discriminator_list, optimizer_generator, latent_space_samples, real_samples_labels, training_mode, epoch, num_discriminators)
       generator_loss = loss_generator.cpu().detach().numpy()
           
-      if generator.name not in loss_values.generator_loss_values:
-        loss_values.generator_loss_values[generator.name] = []
-      loss_values.generator_loss_values[generator.name].append(generator_loss)
+      if generator.module.name not in loss_values.generator_loss_values:
+        loss_values.generator_loss_values[generator.module.name] = []
+      loss_values.generator_loss_values[generator.module.name].append(generator_loss)
       
       if log_wandb:
-        wandb.log({f"{generator.name}_loss": generator_loss})
+        wandb.log({f"{generator.module.name}_loss": generator_loss})
 
       # Update the progress bar
       progress_bar_batch.update()
 
     # Update and Close the progress bar
-    progress_bar_data = {'loss_G': f"{loss_values.generator_loss_values[generator.name][-1]:.5f}"}
+    progress_bar_data = {'loss_G': f"{loss_values.generator_loss_values[generator.module.name][-1]:.5f}"}
     for i, discriminator in enumerate(loss_values.discriminator_loss_values.keys()):
       progress_bar_data[f'loss d_{i}'] = f"{loss_values.discriminator_loss_values[discriminator][-1]:.5f}"
     progress_bar_batch.set_postfix(progress_bar_data)
     progress_bar_batch.close()
     progress_bar_epoch.update()
+
+    if rank == 0:
       
-    # Plot progress
-    if show_training_process:
-      plot_progress.plot(epoch, loss_values)
+      # Plot progress
+      if show_training_process:
+        plot_progress.plot(epoch, loss_values)
 
-    # Save sample data at the specified interval
-    if (epoch + 1) % save_sample_interval == 0:
-      generated_samples = generate_sample(generator, device, 1)
-      generated_samples_list.append(generated_samples)
+      # Save sample data at the specified interval
+      if (epoch + 1) % save_sample_interval == 0:
+        generated_samples = generate_sample(generator, device, 1)
+        generated_samples_list.append(generated_samples)
 
-    # Save checkpoint at the specified interval
-    if (epoch + 1) % checkpoint_interval == 0:
-      save_checkpoint(epoch, checkpoint_folder, model_list, optimizer_list, loss_values)    
+      # Save checkpoint at the specified interval
+      if (epoch + 1) % checkpoint_interval == 0 :
+        save_checkpoint(epoch, checkpoint_folder, model_list, optimizer_list, loss_values)    
 
-    # Plot the evolution of the generator
-    if show_training_evolution:
-      plot_evolution.plot(generated_samples_list[-1], epoch, epoch_i)
+      # Plot the evolution of the generator
+      if show_training_evolution:
+        plot_evolution.plot(generated_samples_list[-1], epoch, epoch_i)
 
   # Close the progress bar
   progress_bar_epoch.close()
 
-  # Finish training
-  print(Fore.GREEN + 'Training finished' + Style.RESET_ALL)
+  if rank == 0:
 
-  # Finish the wandb run
-  if log_wandb:
-    wandb.finish()
+    # Finish training
+    print(Fore.GREEN + 'Training finished' + Style.RESET_ALL)
 
-  # Save final checkpoint
-  save_checkpoint(epochs, checkpoint_folder, model_list, optimizer_list, loss_values)
+    # Finish the wandb run
+    if log_wandb:
+      wandb.finish()
+
+    # Save final checkpoint
+    save_checkpoint(epochs, checkpoint_folder, model_list, optimizer_list, loss_values)
--- src/main.py
+++ src/main.py
@@ -6,136 +6,138 @@ import torch
 import os
 import yaml
 import matplotlib.pyplot as plt
+import torch.multiprocessing as mp
 import wandb
 
-# Load the configuration file
-with open('config.yml', 'r') as file:
-  config = yaml.safe_load(file)
-Hyperparameter = config['Hyperparameter']
-Configuration = config['Configuration']
+def main():
+  # Load the configuration file
+  with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+  Hyperparameter = config['Hyperparameter']
+  Configuration = config['Configuration']
 
-# Start wandb logging
-if Configuration['log_wandb']:
-  wandb_config={
-        "architecture": "HQGAN",
-        "epochs": Hyperparameter['epochs'],
-        "batch_size": Hyperparameter['batch_size'],
-        "training_mode": Configuration['training_mode'],
-        "seed": Configuration['seed'],
-  }
-  wandb.init(
-    project="HybridQuantumGAN",
-    config=wandb_config
-  )
-  # Override local configuration with wandb config (sweep mode)
-  wandb_config = wandb.config
-  for key, value in wandb_config.items():
-    # Update Hyperparameter and Configuration
-    if key in ['epochs', 'batch_size', 'training_mode']:
-      if key in Hyperparameter:
-        Hyperparameter[key] = value
-      elif key in Configuration:
-        Configuration[key] = value
-    # Update model and optimizer learning rate
-    elif key.endswith('learning_rate'):
-      model_name = key.replace('_learning_rate', '')
-      if model_name in Hyperparameter['models']:
-        Hyperparameter['models'][model_name]['learning_rate'] = value
-    
-    # Disable some visualization if using wandb sweep mode
-    if wandb.run.sweep_id is not None:
-      Configuration['show_training_process'] = False
-      Configuration['show_training_evolution'] = False
-      Configuration['show_sample'] = False
-      Configuration['generate_data'] = False
-else:
-  print(Fore.YELLOW + "wandb logging is disabled." + Style.RESET_ALL)
+  # Start wandb logging
+  if Configuration['log_wandb']:
+    wandb_config={
+          "architecture": "HQGAN",
+          "epochs": Hyperparameter['epochs'],
+          "batch_size": Hyperparameter['batch_size'],
+          "training_mode": Configuration['training_mode'],
+          "seed": Configuration['seed'],
+    }
+    wandb.init(
+      project="HybridQuantumGAN",
+      config=wandb_config
+    )
+    # Override local configuration with wandb config (sweep mode)
+    wandb_config = wandb.config
+    for key, value in wandb_config.items():
+      # Update Hyperparameter and Configuration
+      if key in ['epochs', 'batch_size', 'training_mode']:
+        if key in Hyperparameter:
+          Hyperparameter[key] = value
+        elif key in Configuration:
+          Configuration[key] = value
+      # Update model and optimizer learning rate
+      elif key.endswith('learning_rate'):
+        model_name = key.replace('_learning_rate', '')
+        if model_name in Hyperparameter['models']:
+          Hyperparameter['models'][model_name]['learning_rate'] = value
+      
+      # Disable some visualization if using wandb sweep mode
+      if wandb.run.sweep_id is not None:
+        Configuration['show_training_process'] = False
+        Configuration['show_training_evolution'] = False
+        Configuration['show_sample'] = False
+        Configuration['generate_data'] = False
+  else:
+    print(Fore.YELLOW + "wandb logging is disabled." + Style.RESET_ALL)
 
-# Hyperparameters
-model_selector = Hyperparameter['model_selector']
-models = Hyperparameter['models']
-epochs = Hyperparameter['epochs']
-batch_size = Hyperparameter['batch_size']
+  # Hyperparameters
+  model_selector = Hyperparameter['model_selector']
+  models = Hyperparameter['models']
+  epochs = Hyperparameter['epochs']
+  batch_size = Hyperparameter['batch_size']
 
-# Configuration Settings
-device = Configuration['device']
-seed = Configuration['seed']
-save_sample_interval = Configuration['save_sample_interval']
-checkpoint_interval = Configuration['checkpoint_interval']
-training_mode = Configuration['training_mode']
-show_sample = Configuration['show_sample']
-load_checkpoint = Configuration['load_checkpoint']
-training = Configuration['training']
-show_training_process = Configuration['show_training_process']
-show_training_evolution = Configuration['show_training_evolution']
-generate_data = Configuration['generate_data']
-log_wandb = Configuration['log_wandb']
+  # Configuration Settings
+  device = Configuration['device']
+  seed = Configuration['seed']
+  save_sample_interval = Configuration['save_sample_interval']
+  checkpoint_interval = Configuration['checkpoint_interval']
+  training_mode = Configuration['training_mode']
+  show_sample = Configuration['show_sample']
+  load_checkpoint = Configuration['load_checkpoint']
+  training = Configuration['training']
+  world_size = Configuration['world_size']
+  show_training_process = Configuration['show_training_process']
+  show_training_evolution = Configuration['show_training_evolution']
+  generate_data = Configuration['generate_data']
+  log_wandb = Configuration['log_wandb']
 
-# Set up folders path
-script_dir = os.path.dirname(os.path.abspath(__file__))
-data_folder = os.path.join(script_dir, '..', 'data')
-checkpoint_folder = os.path.join(script_dir, 'checkpoints')
+  # Set up distributed training
+  os.environ['MASTER_ADDR'] = 'localhost'
+  os.environ['MASTER_PORT'] = '12355'
 
-# Create required folders
-os.makedirs(checkpoint_folder, exist_ok=True)
-os.makedirs(data_folder, exist_ok=True)
+  # Set up folders path
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  data_folder = os.path.join(script_dir, '..', 'data')
+  checkpoint_folder = os.path.join(script_dir, 'checkpoints')
 
-# Set seed
-if seed is not None:
-  torch.manual_seed(seed)
-  #algorithm_globals.random_seed = seed
+  # Create required folders
+  os.makedirs(checkpoint_folder, exist_ok=True)
+  os.makedirs(data_folder, exist_ok=True)
 
-# Use cuda if available
-device = get_device(device)
+  # Set seed
+  if seed is not None:
+    torch.manual_seed(seed)
+    #algorithm_globals.random_seed = seed
 
-# Load models
-model_list, optimizer_list = get_model(models, model_selector, device)
+  # Use cuda if available
+  device = get_device(device)
 
-# Load data
-train_loader = get_data_loader(batch_size=batch_size, data_folder=data_folder)
+  # Load models
+  model_list, optimizer_list = get_model(models, model_selector, device)
 
-# Plot some training samples
-if show_sample:
-  real_samples, labels = next(iter(train_loader))
-  show_sample_data(real_samples, title='Real Sample', sample_size=16)
+  # Load data
+  train_loader = get_data_loader(batch_size=batch_size, data_folder=data_folder)
 
-# Load checkpoint
-start_epoch, loss_values = 0, None
-if load_checkpoint:
-  start_epoch, loss_values = get_checkpoint(checkpoint_folder=checkpoint_folder, 
-                                            model_list=model_list,
-                                            optimizer_list=optimizer_list)
+  # Plot some training samples
+  if show_sample:
+    real_samples, labels = next(iter(train_loader))
+    show_sample_data(real_samples, title='Real Sample', sample_size=16)
 
-# Update wandb config with models and optimizers
-if log_wandb:
-    wandb.config.update({
-      "num_models": len(model_list),
-      "models": model_list,
-      "optimizer": optimizer_list,
-    })
+  # Load checkpoint
+  start_epoch, loss_values = 0, None
+  if load_checkpoint:
+    start_epoch, loss_values = get_checkpoint(checkpoint_folder=checkpoint_folder, 
+                                              model_list=model_list,
+                                              optimizer_list=optimizer_list)
 
-# Train model
-if training and epochs != start_epoch:
-  train_model(device=device, 
-              epochs=epochs,
-              train_loader=train_loader,
-              model_list=model_list,
-              optimizer_list=optimizer_list,
-              checkpoint_folder=checkpoint_folder,
-              start_epoch=start_epoch,
-              loss_values=loss_values,
-              checkpoint_interval=checkpoint_interval,
-              save_sample_interval=save_sample_interval,
-              training_mode=training_mode,
-              log_wandb=log_wandb,
-              show_training_process=show_training_process,
-              show_training_evolution=show_training_evolution) 
+  # Update wandb config with models and optimizers
+  if log_wandb:
+      wandb.config.update({
+        "num_models": len(model_list),
+        "models": model_list,
+        "optimizer": optimizer_list,
+      })
 
-# Generate sample
-if generate_data:
-  generated_sample = generate_sample(model_list[0], device, sample_size=16)
-  show_sample_data(generated_sample, title='Generated Sample')
+  # Train model using multiprocessing
+  if training and epochs != start_epoch:
+    mp.spawn(
+          train_model,
+          args=(world_size, device, epochs, train_loader, model_list, optimizer_list, checkpoint_folder, log_wandb, show_training_process, show_training_evolution, save_sample_interval, start_epoch, checkpoint_interval, training_mode, loss_values),
+          nprocs=world_size,
+          join=True
+    )
 
-# Keep the plot open
-plt.ion()
-plt.show(block=True)
+  # Generate sample
+  if generate_data:
+    generated_sample = generate_sample(model_list[0], device, sample_size=16)
+    show_sample_data(generated_sample, title='Generated Sample')
+
+  # Keep the plot open
+  plt.ion()
+  plt.show(block=True)
+
+if __name__ == '__main__':
+    main()
